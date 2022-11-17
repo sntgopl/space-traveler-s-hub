1dee887d7bacc37cadca84f38a5d96b11e3ba651
sntgopl <105621110+sntgopl@users.noreply.github.com>
2022-11-17 10:17:06 -0300

    Add status update to react component and button dispatch action

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,22 +1,54 @@
 import PropTypes from 'prop-types';
-import { Button } from 'react-bootstrap';
+import { Button, Badge } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+import { joinMission, leaveMission } from '../redux/missions/missions';
 
 const Mission = ({
-  name, description,
-}) => (
-  <tr>
-    <td>
-      {name}
-    </td>
-    <td>{description}</td>
-    <td>Non-Active</td>
-    <td><Button variant="outline-secondary">Join Mission</Button></td>
-  </tr>
-);
+  name, description, id, reserved,
+}) => {
+  const dispatch = useDispatch();
+  return (
+    <tr>
+      <td>
+        {name}
+      </td>
+      <td>{description}</td>
+      <td>
+        {!reserved && (
+          <Badge bg="secondary">Non-Active</Badge>
+        )}
+        {reserved && (
+          <Badge bg="primary">Active</Badge>
+        )}
+      </td>
+      <td>
+        {!reserved && (
+        <Button
+          onClick={() => dispatch(joinMission(id))}
+          variant="outline-secondary"
+        >
+          Join Mission
+        </Button>
+        )}
+        {reserved && (
+        <Button
+          onClick={() => dispatch(leaveMission(id))}
+          variant="outline-danger"
+        >
+          Join Mission
+        </Button>
+        )}
+
+      </td>
+    </tr>
+  );
+};
 
 Mission.propTypes = {
   name: PropTypes.node.isRequired,
   description: PropTypes.node.isRequired,
+  id: PropTypes.node.isRequired,
+  reserved: PropTypes.node.isRequired,
 };
 
 export default Mission;
