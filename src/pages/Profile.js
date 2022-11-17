bfd868a36cb6106f04cff67316defc4f3eec46c0
IngDavidTM <david-1011@hotmail.com>
2022-11-17 10:01:05 -0500

    add filter to profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,29 +4,21 @@ import '../stylesheets/Profile.css';
 
 const Profile = () => {
   const missions = useSelector((state) => state.mission);
+  const resultMissions = missions.filter((mission) => mission.reserved);
   const rockets = useSelector((state) => state.rocket);
+  const resultRockets = rockets.filter((rocket) => rocket.reserved);
   return (
     <div className="profile">
       <Table bordered>
         <h2>My Missions</h2>
         <tbody>
-          {missions.map((mission) => {
-            if (mission.reserved) {
-              return (<tr key={mission.id}>{mission.name}</tr>);
-            }
-            return false;
-          })}
+          {resultMissions.map((mission) => (<tr key={mission.id}>{mission.name}</tr>))}
         </tbody>
       </Table>
       <Table bordered>
         <h2>My Rockets</h2>
         <tbody>
-          {rockets.map((rocket) => {
-            if (rocket.reserved) {
-              return (<tr key={rocket.id}>{rocket.name}</tr>);
-            }
-            return false;
-          })}
+          {resultRockets.map((rocket) => (<tr key={rocket.id}>{rocket.name}</tr>))}
         </tbody>
       </Table>
     </div>
