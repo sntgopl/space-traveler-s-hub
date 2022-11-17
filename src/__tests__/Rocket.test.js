8a46389d8f20e7a1d3af81ea868e8f08bf5fe906
IngDavidTM <david-1011@hotmail.com>
2022-11-17 12:51:51 -0500

    add test to rocket with testing library

--- src/__tests__/Rocket.test.js
+++ src/__tests__/Rocket.test.js
@@ -1,4 +1,6 @@
 import { Provider } from 'react-redux';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import renderer from 'react-test-renderer';
 import Rocket from '../components/Rocket';
 import store from '../redux/configureStore';
@@ -19,5 +21,41 @@ describe('test snapshot', () => {
       </Provider>,
     ).toJSON();
     expect(renderRocketContainer).toMatchSnapshot();
+  });
+});
+
+describe('test with testing library', () => {
+  it('should change to cancel reservation', () => {
+    render(
+      <Provider store={store}>
+        <Rocket
+          key={1}
+          id={1}
+          name="Rocket test"
+          type="Rocker"
+          img="url test"
+          description="It is a rocket"
+          reserved
+        />
+      </Provider>,
+    );
+    expect(screen.getByText('Cancel Reservation')).toBeInTheDocument();
+  });
+
+  it('should change to reserve rocket', () => {
+    render(
+      <Provider store={store}>
+        <Rocket
+          key={1}
+          id={1}
+          name="Rocket test"
+          type="Rocker"
+          img="url test"
+          description="It is a rocket"
+          reserved={false}
+        />
+      </Provider>,
+    );
+    expect(screen.getByText('Reserve Rocket')).toBeInTheDocument();
   });
 });
